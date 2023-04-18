3c640149edd669f533e08893399c306e5f5de49f
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-04-18 18:13:21 +0100

    Add athletes data dynamically

---
+++ scripts/data.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: 'Abdoul Razak Alphaga',
+    sport: 'Taikwondo',
+    history:
+      'When Issoufou was 7, his father forbade him from taking taekwondo after a cousin died from an injury in a fight ...',
+    image: '../images/alphaga.jpg',
+  },
+
+  {
+    name: 'Fabrice ZANGO HUGUES',
+    sport: "Men's Triple Jump",
+    history:
+      "Burkina Faso's Hugues Fabrice Zango on creating history for Africa at the 2022 Track and Field World Championships",
+    image: '../images/fabrice.jpeg',
+  },
+
+  {
+    name: 'Usain Bolt',
+    sport: 'Track and field',
+    history:
+      'Usain St. Leo Bolt (born 21 August 1986) is a Jamaican retired sprinter, widely considered to be the greatest sprinter of all time. He is the world record holder in the 100 metres, 200 metres, and 4 × 100 metres relay.',
+    image: '../images/usain.jpeg',
+  },
+
+  {
+    name: 'Tobi Amusan',
+    sport: "Women's 100m Hurdles",
+    history:
+      'The 25-year-old was competing in the very first race of Day 10 of the championships when she breezed past the line in the new world record of 12.12,',
+    image: '../images/Tobi.webp',
+  },
+
+  {
+    name: 'Feryal Abdelaziz',
+    sport: 'Karate ',
+    history:
+      'Feryal Abdelaziz, Egypt, Gold, Karate Women’s Kumite +61kg',
+    image: '../images/Feryal.webp',
+  },
+
+  {
+    name: 'Peruth Chemutai',
+    sport: 'Women’s 3,000m Steeplechase',
+    history:
+      'Peruth Chemutai, Uganda, Gold (first Ugandan woman to win an Olympic gold medal in any sport), Women’s 3,000m Steeplechase',
+    image: '../images/Peruth.webp',
+  },
+
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,45 @@
-const menuBars = document.querySelector('.menu-bars')
-const sidebar = document.querySelector('.sidebar')
-const body = document.querySelector('body')
-const cross = document.querySelector('.cross')
+import datas from './data.js';
 
+const menuBars = document.querySelector('.menu-bars');
+const sidebar = document.querySelector('.sidebar');
+const body = document.querySelector('body');
+const cross = document.querySelector('.cross');
+const athleteWrapper = document.querySelector('.athlete-wrapper');
+
+// side menu
 const closeSide = () => {
-    sidebar.classList.remove('open-side')
-    body.style.overflow = 'unset'
-}
+  sidebar.classList.remove('open-side');
+  body.style.overflow = 'unset';
+};
 
 const openSide = () => {
-    sidebar.classList.add('open-side')
-    body.style.overflow = 'hidden'
+  sidebar.classList.add('open-side');
+  body.style.overflow = 'hidden';
+};
+
+menuBars.addEventListener('click', openSide);
+cross.addEventListener('click', closeSide);
+// side menu
+
+// famous athlete
+for (let index = 0; index < datas.length; index += 1) {
+  const data = datas[index];
+  const athlete = document.createElement('div');
+  athlete.classList.add('athlete');
+  athlete.innerHTML = `
+    <div class="athlete-picture">
+                        <img src="./images/mozaic_thumbnail.png" class="picture-corner" />
+                        <img src=${data.image} class="athlete-image" />
+                    </div>
+
+                    <div class="athlete-infos">
+                        <h4 class="athlete-name">${data.name}</h4>
+                        <h5 class="athlete-sport">${data.sport}</h5>
+                        <hr>
+                        <p class="athlete-history">${data.history}</p>
+                    </div>
+    `;
+  athleteWrapper.appendChild(athlete);
 }
 
-menuBars.addEventListener('click',openSide)
-cross.addEventListener('click', closeSide)
+// famous athlete
